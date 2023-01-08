f7f11867a9c60cc22b2cc3139cc5bc8fe738ad55
Deimos <deimoshall@gmail.com>
2023-01-08 12:25:47 -0600

    Add box layout

---
+++ app/src/main/java/com/deimos/jetpackcomposelayouts/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.deimos.jetpackcomposelayouts
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.deimos.jetpackcomposelayouts.ui.theme.JetpackComposeLayoutsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeLayoutsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyBox()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyBox() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .width(300.dp)
+                .height(300.dp)
+                .background(Color.Cyan)
+                .padding(15.dp),
+            Alignment.CenterEnd
+        ) {
+            Button(onClick = {  }) {
+                Text(text = "Button")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeLayoutsTheme {
+        MyBox()
+    }
+}
